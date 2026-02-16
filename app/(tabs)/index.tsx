7ccca7a66efaf59ff3b25c161bbbc41ee8de10ab
Ross <snapper-appeals4c@icloud.com>
2026-02-16 16:47:57 +0200

    Remove mock charges

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,17 +9,7 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function PointOfSale() {
     const safeAreaInsets = useSafeAreaInsets();
-    const [charges, setCharges] = useState<Charge[]>([
-        { amount: 100.2, key: "1" },
-        { amount: 99.99, key: "2" },
-        { amount: 5.8, key: "3" },
-        { amount: 13.0, key: "4" },
-        { amount: 702.0, key: "5" },
-        { amount: 90823, key: "6" },
-        { amount: 301, key: "7" },
-        { amount: 17.27, key: "8" },
-        { amount: 10.05, key: "9" },
-    ]);
+    const [charges, setCharges] = useState<Charge[]>([]);
     const backgroundColor = useThemeColors("background");
 
     const onAddCharge = (amount: number) => {
