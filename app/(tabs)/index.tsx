d67b3fd07be087ce15fb2491679d3c6de417ec76
Ross <snapper-appeals4c@icloud.com>
2026-02-16 13:26:01 +0200

    Add mock charges for testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,17 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function PointOfSale() {
     const safeAreaInsets = useSafeAreaInsets();
-    const [charges, setCharges] = useState<Charge[]>([]);
+    const [charges, setCharges] = useState<Charge[]>([
+        { amount: 100.2, key: "1" },
+        { amount: 99.99, key: "2" },
+        { amount: 5.8, key: "3" },
+        { amount: 13.0, key: "4" },
+        { amount: 702.0, key: "5" },
+        { amount: 90823, key: "6" },
+        { amount: 301, key: "7" },
+        { amount: 17.27, key: "8" },
+        { amount: 10.05, key: "9" },
+    ]);
 
     const onAddCharge = (amount: number) => {
         setCharges([
