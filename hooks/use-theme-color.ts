31913d8fd6c17524da4bcc49e758318a8cb72aa4
Ross <snapper-appeals4c@icloud.com>
2026-02-16 09:45:54 +0200

    Remove comment

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -2,11 +2,6 @@ import { useColorScheme } from "react-native";
 import { Colours } from "@/constants/theme";
 import { ColourName } from "@/constants/types";
 
-// export function useThemeColors(colorName: ColourName) {
-//     const theme = useColorScheme() ?? "light";
-//     return Colours[theme][colorName];
-// }
-
 export function useThemeColors(colorName: ColourName): string;
 export function useThemeColors(...colorNames: ColourName[]): string[];
 export function useThemeColors(...colorNames: ColourName[]): string | string[] {
